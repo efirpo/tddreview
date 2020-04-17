47d4fd53cd555520fe8449ebf7728881e5906782
Ethan Firpo <efirpo@gmail.com>
2020-04-17 13:33:34 -0700

    refactor age calculation tests to account for new methods

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -16,34 +16,34 @@ describe('solar system age calculations', () => {
     solarAges.calculateAgeInDays();
     expect(solarAges.ageInDays).toEqual(12908);
   });
-  // test('should calculate age in Mercurial years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.splitDate("1984-12-15");
-  //   solarAges.calculateAgeInDays();
-  //   solarAges.mercuryAge();
-  //   expect(solarAges.mercury).toEqual(147);
-  // });
-  // test('should calculate age in Venusian years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.splitDate("1984-12-15");
-  //   solarAges.calculateAgeInDays();
-  //   solarAges.venusAge();
-  //   expect(solarAges.venus).toEqual(57)
-  // });
-  // test('should calculate age in Martian years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.splitDate("1984-12-15");
-  //   solarAges.calculateAgeInDays();
-  //   solarAges.marsAge();
-  //   expect(solarAges.mars).toEqual(19)
-  // });
-  // test('should calculate age in Jovian years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.splitDate("1984-12-15");
-  //   solarAges.calculateAgeInDays();
-  //   solarAges.jupiterAge();
-  //   expect(solarAges.jupiter).toEqual(3)
-  // });
+  test('should calculate age in Mercurial years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.mercuryAge();
+    expect(solarAges.mercury).toEqual(147);
+  });
+  test('should calculate age in Venusian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.venusAge();
+    expect(solarAges.venus).toEqual(57)
+  });
+  test('should calculate age in Martian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.marsAge();
+    expect(solarAges.mars).toEqual(19)
+  });
+  test('should calculate age in Jovian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.jupiterAge();
+    expect(solarAges.jupiter).toEqual(3)
+  });
   // test('should calculate remaing life expected in Earth years', () => {
   //   let solarAges = new solarAgeCalculator();
   //   solarAges.splitDate("1984-12-15")
@@ -53,11 +53,13 @@ describe('solar system age calculations', () => {
   // })
   // test('should calculate remaining life expectancy in Mercurial years', () => {
   //   let solarAges = new solarAgeCalculator();
+  //   solarAges.splitDate("1984-12-15")
+  //   solarAges.calculateAgeInDays();
   //   solarAges.calculateLifeExpectancy(71);
   //   expect(solarAges.mercExpect).toEqual(147)
   // })
   // test('should calculate remaining life expectancy in Venusian years', () => {
-  //   let solarAges = new solarAgeCalculator(1984, 12, 15);
+  //   let solarAges = new solarAgeCalculator();
   //   solarAges.calculateLifeExpectancy(71);
   //   expect(solarAges.venExpect).toEqual(58)
   // })
