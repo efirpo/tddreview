493b30363dc03aa0a71b35cd4af2cd9f143109a2
Ethan Firpo <efirpo@gmail.com>
2020-04-17 11:24:08 -0700

    implement fail test for remaining life expectance

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -3,26 +3,31 @@ import { solarAgeCalculator } from './../src/galactic.js'
 describe('solar system age calculations', () => {
   test('should create a solar-age object', () => {
     let solarAges = new solarAgeCalculator(1984, 12, 15);
-    expect(solarAges.ageInDays).toEqual(12876);
-  })
+    expect(solarAges.ageInDays).toEqual(12907);
+  });
   test('should calculate age in Mercurial years', () => {
     let solarAges = new solarAgeCalculator(1984, 12, 15);
     solarAges.mercuryAge();
-    expect(solarAges.mercury).toEqual(146);
-  })
+    expect(solarAges.mercury).toEqual(147);
+  });
   test('should calculate age in Venusian years', () => {
     let solarAges = new solarAgeCalculator(1984, 12, 15);
     solarAges.venusAge();
     expect(solarAges.venus).toEqual(57)
-  })
+  });
   test('should calculate age in Martian years', () => {
     let solarAges = new solarAgeCalculator(1984, 12, 15);
     solarAges.marsAge();
     expect(solarAges.mars).toEqual(19)
-  })
+  });
   test('should calculate age in Jovian years', () => {
     let solarAges = new solarAgeCalculator(1984, 12, 15);
     solarAges.jupiterAge();
     expect(solarAges.jupiter).toEqual(3)
+  });
+  test('should calculate remaing life expected in Earth years', () => {
+    let solarAges = new solarAgeCalculator(1984, 12, 15);
+    solarAges.calculateLifeExpectancy();
+    expect(solarAges.lifeExpect).toEqual(36)
   })
-})
+});
--- src/galactic.js
+++ src/galactic.js
@@ -1,29 +1,29 @@
-// These calculations are not quite correct, and I cannot figure out why. Using my birthdate of "1984, 12, 15" returns 12876 Earth days when it should be 12906, which is a greater difference than can be explained by Leap Years or any other such assumption I can think of to account for it. Something appears fishy under the hood of Date(), and it means having to work backwards with my expected results. 
-
 export class solarAgeCalculator {
   constructor(year, month, day) {
-    this.ageInDays = Math.round((new Date() - new Date(year, month, day)) / (1000 * 60 * 60 * 24));
+    this.ageInDays = Math.round((new Date() - new Date(year, (month - 1), day)) / (1000 * 60 * 60 * 24));
     this.mercury;
     this.venus;
     this.mars;
     this.jupiter;
-  };
-  // calculations simplified by only using years and ignoring leap years
+  }
 
   mercuryAge() {
-    this.mercury = Math.round((this.ageInDays / 88))
-  };
+    this.mercury = Math.round((this.ageInDays / 88));
+  }
 
   venusAge() {
-    this.venus = Math.round((this.ageInDays / 225))
-  };
+    this.venus = Math.round((this.ageInDays / 225));
+  }
 
   marsAge() {
-    this.mars = Math.round((this.ageInDays / 687))
-  };
+    this.mars = Math.round((this.ageInDays / 687));
+  }
 
   jupiterAge() {
-    this.jupiter = Math.round((this.ageInDays / 365) / 12)
-  };
+    this.jupiter = Math.round((this.ageInDays / 365) / 12);
+  }
 
+  calculateLifeExpectancy() {
+
+  }
 }
