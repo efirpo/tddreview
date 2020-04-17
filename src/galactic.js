8091eb7c3c5d523aaebdee6f3f93b98b261b5492
Ethan Firpo <efirpo@gmail.com>
2020-04-17 09:47:24 -0700

    start over using calculations based on how many days since a person's birth

--- src/galactic.js
+++ src/galactic.js
@@ -1,26 +1,27 @@
 export class solarAgeCalculator {
-  constructor(age) {
-    this.age = age;
+  constructor(year, month, day) {
+    this.ageinDays = Math.round((new Date() - new Date(year, month, day)) / (1000 * 60 * 60 * 24));
     this.mercury;
     this.venus;
     this.mars;
     this.jupiter;
-  }
+  };
   // calculations simplified by only using years and ignoring leap years
 
   mercuryAge() {
-    this.mercury = Math.round((this.age * 365) / 88)
-  }
+    // this.mercury = Math.round((this.ageinDays / 88))
+  };
 
-  venusAge() {
-    this.venus = Math.round((this.age * 365) / 225)
-  }
+  // venusAge() {
+  //   this.venus = Math.round((this.age * 365) / 225)
+  // };
 
-  marsAge() {
-    this.mars = Math.round((this.age) * 365 / 687)
-  }
+  // marsAge() {
+  //   this.mars = Math.round((this.age) * 365 / 687)
+  // };
 
-  jupiterAge() {
-    this.jupiter = Math.round((this.age) / 12)
-  }
+  // jupiterAge() {
+  //   this.jupiter = Math.round((this.age) / 12)
+  // };
+
 }
