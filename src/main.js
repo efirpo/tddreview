98806c745fc6e4878e580890d3794946efcea994
Ethan Firpo <efirpo@gmail.com>
2020-04-17 15:36:16 -0700

    make quote marks consistent throughout document

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import $ from "jquery";
+import $ from 'jquery';
 import { solarAgeCalculator } from './galactic';
 
 $(document).ready(function () {
-  $("#age-form").submit(function (event) {
+  $('#age-form').submit(function (event) {
     event.preventDefault;
     let ages = new solarAgeCalculator();
     ages.splitDate($('#bday-input').val());
@@ -14,17 +14,17 @@ $(document).ready(function () {
     ages.venusAge();
     ages.marsAge();
     ages.jupiterAge();
-    ages.calculateLifeExpectancy();
-    $("#age-in-days").html(ages.ageInDays);
-    $("#age-merc").html(ages.mercury);
-    $("age-ven").html(ages.venus);
-    $("#age-mars").html(ages.mars);
-    $("#age-jup").html(ages.jupiter);
-    $("life-merc").html(ages.mercExpect);
-    $("#life-ven").html(ages.venExpect);
-    $("#life-mars").html(ages.marsExpect);
-    $("#life-jup").html(ages.jupExpect);
-    $("#results").fadeIn(100).addClass("animated fadeInRight");
+    ages.calculateLifeExpectancy($('#demographic').val());
+    $('#age-in-days').html(ages.ageInDays);
+    $('#age-merc').html(ages.mercury);
+    $('age-ven').html(ages.venus);
+    $('#age-mars').html(ages.mars);
+    $('#age-jup').html(ages.jupiter);
+    $('life-merc').html(ages.mercExpect);
+    $('#life-ven').html(ages.venExpect);
+    $('#life-mars').html(ages.marsExpect);
+    $('#life-jup').html(ages.jupExpect);
+    $('#results').fadeIn(100).addClass('animated fadeInRight');
   });
 
 });
