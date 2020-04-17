4222bd2d66dc7d2ffbb93a74a2e5f90e85eec29a
Ethan Firpo <efirpo@gmail.com>
2020-04-17 08:15:48 -0700

    implement fail test for creating solar ages object

---
+++ __tests__/galactic.test.js
@@ -0,0 +1,8 @@
+import { solarAgeCalculator } from './../src/galactic.js'
+
+describe('solar system age calculations', () => {
+  test('should create a solar-age object', () => {
+    let solarAges = new solarAgeCalculator(35);
+    expect(solarAges.age).toEqual(35);
+  })
+})
