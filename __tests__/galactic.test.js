ee081666c7a0e2405b6bc7c184bff098ac9a0f8a
Ethan Firpo <efirpo@gmail.com>
2020-04-17 13:37:21 -0700

    refactor other life expectancy tests

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -51,26 +51,32 @@ describe('solar system age calculations', () => {
     solarAges.calculateLifeExpectancy(71);
     expect(solarAges.lifeExpect).toEqual(36)
   })
-  // test('should calculate remaining life expectancy in Mercurial years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.splitDate("1984-12-15")
-  //   solarAges.calculateAgeInDays();
-  //   solarAges.calculateLifeExpectancy(71);
-  //   expect(solarAges.mercExpect).toEqual(147)
-  // })
-  // test('should calculate remaining life expectancy in Venusian years', () => {
-  //   let solarAges = new solarAgeCalculator();
-  //   solarAges.calculateLifeExpectancy(71);
-  //   expect(solarAges.venExpect).toEqual(58)
-  // })
-  // test('should calculate remaining life expectancy in Martian years', () => {
-  //   let solarAges = new solarAgeCalculator(1984, 12, 15);
-  //   solarAges.calculateLifeExpectancy(71);
-  //   expect(solarAges.marsExpect).toEqual(19)
-  // })
-  // test('should calculate remaining life expectany in Jovian years', () => {
-  //   let solarAges = new solarAgeCalculator(1984, 12, 15);
-  //   solarAges.calculateLifeExpectancy(71);
-  //   expect(solarAges.jupExpect).toEqual(3)
-  // })
+  test('should calculate remaining life expectancy in Mercurial years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15")
+    solarAges.calculateAgeInDays();
+    solarAges.calculateLifeExpectancy(71);
+    expect(solarAges.mercExpect).toEqual(147)
+  })
+  test('should calculate remaining life expectancy in Venusian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.calculateLifeExpectancy(71);
+    expect(solarAges.venExpect).toEqual(58)
+  })
+  test('should calculate remaining life expectancy in Martian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.calculateLifeExpectancy(71);
+    expect(solarAges.marsExpect).toEqual(19)
+  })
+  test('should calculate remaining life expectany in Jovian years', () => {
+    let solarAges = new solarAgeCalculator();
+    solarAges.splitDate("1984-12-15");
+    solarAges.calculateAgeInDays();
+    solarAges.calculateLifeExpectancy(71);
+    expect(solarAges.jupExpect).toEqual(3)
+  })
 });
